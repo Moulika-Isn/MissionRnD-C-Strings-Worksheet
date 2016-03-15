23408269cc76468aa268970e1877e62d1d14456f
Moulika-Isn <Moulika-Isn@missionrnd.com>
2016-03-15 22:38:05 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -12,6 +12,44 @@ NOTES: Don't create new string.
 #include <Stdio.h>
 #include <string.h>
 
+void swap(char *p, char *q)
+{
+	char temp;
+	temp = *p;
+	*p = *q;
+	*q = temp;
+}
+void reverse(char str[])
+{
+	int i = 0, j, k = 0;
+	int m = 0, n;
+	while (str[i] != '\0')
+	{
+		if (str[i] == 32)
+		{
+			m = 1;
+			n = (i - k) / 2;
+			for (j = k; m <= n; j++)
+			{
+				swap(&str[j], &str[i - m]);
+				m++;
+			}
+			k = i + 1;
+
+		}
+		i++;
+	}
+	m = 1;
+	n = (i - k) / 2;
+	for (j = k; m <= n; j++)
+	{
+		swap(&str[j], &str[i - m]);
+		m++;
+	}
+}
 void str_words_in_rev(char *input, int len){
-	
+	int i = 0;
+	for (i = 0; i < len / 2; i++)
+		swap(input + i, input + len - i - 1);
+	reverse(input);
 }
